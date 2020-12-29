e3bf25df74366505426817d628c05e067e058c95
akshay_s <akshays@symplr.com>
2020-12-28 23:49:30 -0800

    Set up app for heroku

--- src/app.js
+++ src/app.js
@@ -90,6 +90,6 @@ app.get('*', (req,res) => {
         errorMSG: 'Generic page not found'
     });
 })
-app.listen(port, () => {
-    console.log('Server started');
-});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
